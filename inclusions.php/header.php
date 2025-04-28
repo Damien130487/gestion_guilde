919d5354ad256701dc8052df675c92670031af10
damien <quesnel.damien13@outlook.fr>
2025-04-28 16:04:52 +0200

    modif

---
+++ inclusions.php/header.php
@@ -0,0 +1,9 @@
+           <header>
+              <div class="header-items" id="embleme"><img src="img/EmblemGuild.png" /></div>
+              <div class="header-items" id="divTitle">
+                  <h1>FICHTRE DES RAIDS</h1>
+                 
+                  <h2>Fruit de Mer Online</h2>
+              </div>
+              <div class="header-items" id="icon"><a href="#"><img src="img/download-ICON.png" /></a></div>
+           </header>
